deed985b6c7542d99b65ee367560f0381c7335b3
Shaked Levy <shakedlv0@gmail.com>
2024-11-01 08:19:08 +0200

    Added basic landing page layout

---
+++ src/components/landing-page-header/landing-page-header.tsx
@@ -0,0 +1,16 @@
+import './landing-page-header.css'
+
+function LandingPageHeader() {
+    return (
+        <header>
+                <h1>Kitchen Management Software For Sustainable Food Service Management</h1>
+                <p>Recipe Management. Menu Planning. Restaurant Inventory Management. Food Production And Requisitions. Multi-Unit Restaurant Management. Sustainability.</p>
+                <br/>
+                    <button className='btn'>Join Us !</button>
+        </header>
+
+
+    )
+}
+
+export default LandingPageHeader
